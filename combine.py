b0a11550d27d196b18d210bdea23129714763433
YujieLu10 <yujielu10@gmail.com>
2021-06-25 12:37:54 -0400

    delete

--- combine.py
+++
@@ -1,7 +0,0 @@
-import os
-str_name=[]
-for file in os.listdir("mj_envs/mj_envs/envs/hand_manipulation_suite/combine/objects"):
-    str_name.append(file[:-4])
-    # if "xml" in file:
-    #     print("<include file=\"objects/{}.xml\" />".format(file))
-print(str_name)
