caee71694e39ca8a02360fec72c29d70207fd273
YujieLu10 <yujielu10@gmail.com>
2021-06-25 12:36:23 -0400

    delete

--- tool/if-net/generate.sh
+++
@@ -1,1 +0,0 @@
-python generate.py -std_dev 0.1 0.01 -res 32 -m ShapeNet32Vox -checkpoint 10 -batch_points 400000
--- tool/reconstruct_mesh_vis.py
+++
@@ -1,6 +0,0 @@
-import open3d as o3d
-import os
-
-
-mesh = o3d.io.read_triangle_mesh("export_pathsurface_reconstruction.off")
-o3d.visualization.draw_geometries([mesh])
